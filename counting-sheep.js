045cc681d3bdd2be782a6a88efdc069d8aa98089
Adrian <adriantoddross@gmail.com>
2018-04-02 11:46:10 -0400

    stub counting-sheep drill

---
+++ counting-sheep.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// What is the input to the program
+// What is the output of the program
+// What is the input to each recursive calls
+// What is the output of each recursive calls
+
+// Counting Sheep
+// Write a recursive function that counts how many sheep jumps over the fence. Your program should take a number as an input. That number should be the number of sheep you have. The function should display the number along with the message "Another sheep jump over the fence" until no more sheep are left.
+
+// Input: 3
+// Output:
+// 3 - Another sheep jump over the fence
+// 2 - Another sheep jump over the fence
+// 1 - Another sheep jump over the fence
+
+// The input is a number
+// The output is a string
+// The recursive input is a number
+// The recursive output is a string
